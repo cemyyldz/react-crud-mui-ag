e916d82b001af4a399d26a8e79b151b98c50a261
Oğulcem Yıldız <91990302+cemyyldz@users.noreply.github.com>
2026-01-31 23:49:51 +0300

    create işlemleri tamamlandı

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,19 @@ import axios from 'axios'
 import { AgGridReact } from 'ag-grid-react';
 import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
 ModuleRegistry.registerModules([ AllCommunityModule ]);
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import { Container, Typography, Box, Button, TextField, Checkbox, FormControlLabel } from '@mui/material';
 
 function App() {
   const [users,setUsers] = useState([]);
+
+  const [formData, setFormData]= useState([{
+    isim:"",
+    soyisim:"",
+    email:"",
+    telefon:"",
+    aciklama:"",
+    isActive:false
+  }]);
 
   const [columnDefs] = useState([
     {field: 'id', headerName:'ID', width:70},
@@ -15,6 +23,7 @@ function App() {
     {field: 'soyisim', headerName:'Soyisim', flex:1},
     {field: 'email', headerName:'E-mail', flex:1},
     {field: 'telefon', headerName:'Telefon', flex:1},
+    {field: 'aciklama', headerName:'Açıklama', flex:1},
     {field: 'isActive',
       headerName:'Üyelik Durumu',
       width:100,
@@ -42,17 +51,97 @@ function App() {
   getUsers();
  },[]);
 
+ const handleChange = (e) => {
+  const value = e.target.type ==='checkbox' ? e.target.checked : e.target.value;
+
+  setFormData({
+    ...formData,
+    [e.target.name]: value
+  });
+
+ };
+
+ const handleSave = async () => {
+
+  try{
+    await axios.post("https://697e36ac97386252a26a2c01.mockapi.io/kullanici",formData);
+    getUsers;
+    setFormData({isim:"",
+    soyisim:"",
+    email:"",
+    telefon:"",
+    aciklama:"",
+    isActive:false});
+    alert("Kayıt Başarılıdırr.")
+  }
+  catch(error){
+    console.log("Kayıt oluşturulurken bir hata oluştu.",error);
+  }
+ };
+
 return (
-  <Container maxWidth="lg" sx={{ marginTop: 4 }}>
-    <Typography variant="h4" component="h1" gutterBottom align="center">
-        Kullanıcı Yönetim Paneli
+<Container maxWidth="lg" sx={{ marginTop: 4 }}>
+      <Typography variant="h4" gutterBottom align="center">
+        Personel Listesi
       </Typography>
+      <Box sx={{ display: 'flex', gap: 2, marginBottom: 4, flexWrap: 'wrap',backgroundColor:'primary.light' }}>
+        <TextField 
+          label="İsim" 
+          name="isim" 
+          variant="outlined" 
+          size="small" 
+          value={formData.isim} 
+          onChange={handleChange} 
+        />
+        <TextField 
+          label="Soyisim" 
+          name="soyisim" 
+          variant="outlined" 
+          size="small" 
+          value={formData.soyisim} 
+          onChange={handleChange} 
+        />
+        <TextField 
+          label="Email" 
+          name="email" 
+          variant="outlined" 
+          size="small" 
+          value={formData.email} 
+          onChange={handleChange} 
+        />
+        <TextField 
+          label="Telefon" 
+          name="telefon" 
+          variant="outlined" 
+          size="small" 
+          value={formData.telefon} 
+          onChange={handleChange} 
+        />
+        <TextField 
+          label="Açıklama" 
+          name="aciklama" 
+          variant="outlined" 
+          size="small" 
+          value={formData.aciklama} 
+          onChange={handleChange} 
+        />
+        <FormControlLabel
+            control={<Checkbox checked={formData.isActive} onChange={handleChange} name="isActive" />}
+            label="Aktif"
+          />
+        <Button variant="contained" color="primary"  onClick={handleSave} >
+          EKLE
+        </Button>
+      </Box>
+
+
       <div className="ag-theme-quartz" style={{ height: 500, width: '100%' }}>
         <AgGridReact
           rowData={users}          
           columnDefs={columnDefs}  
           pagination={true}       
-          paginationPageSize={10} 
+          paginationPageSize={10}
+          paginationPageSizeSelector={[10, 20, 50]} 
         />
       </div>
 
