4870faf0562e4d7d3fbf7f4933232767763e53aa
Oğulcem Yıldız <91990302+cemyyldz@users.noreply.github.com>
2026-01-31 22:52:58 +0300

    ag ile ekranda gosterme

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,27 @@
 import { useState , useEffect} from 'react'
 import axios from 'axios'
-import './App.css'
+import { AgGridReact } from 'ag-grid-react';
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+ModuleRegistry.registerModules([ AllCommunityModule ]);
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 function App() {
   const [users,setUsers] = useState([]);
+
+  const [columnDefs] = useState([
+    {field: 'id', headerName:'ID', width:70},
+    {field: 'isim', headerName:'İsim', flex:1},
+    {field: 'soyisim', headerName:'Soyisim', flex:1},
+    {field: 'email', headerName:'E-mail', flex:1},
+    {field: 'telefon', headerName:'Telefon', flex:1},
+    {field: 'isActive',
+      headerName:'Üyelik Durumu',
+      width:100,
+      cellRenderer:(params) => params.value ? "Aktif" : "Pasif" 
+
+    }
+  ]);
 
   const getUsers = async () => {
     try{
@@ -20,14 +38,28 @@ function App() {
     
   };
 
-
-
-
-
-
  useEffect(()=>{
   getUsers();
  },[]);
 
+return (
+  <Container maxWidth="lg" sx={{ marginTop: 4 }}>
+    <Typography variant="h4" component="h1" gutterBottom align="center">
+        Kullanıcı Yönetim Paneli
+      </Typography>
+      <div className="ag-theme-quartz" style={{ height: 500, width: '100%' }}>
+        <AgGridReact
+          rowData={users}          
+          columnDefs={columnDefs}  
+          pagination={true}       
+          paginationPageSize={10} 
+        />
+      </div>
+
+    </Container>
+  
+);
+
 }
+
 export default App
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'ag-grid-community/styles/ag-grid.css'; 
+import 'ag-grid-community/styles/ag-theme-quartz.css'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
